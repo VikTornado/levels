7778f3fd77194e0d5e1839c6f0b09937b2bfd4c8
React <3809164@gmail.com>
2023-12-12 12:29:46 +0200

    Levels

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Basic from "./Components/Basic";
+import Intermediate from "./Components/Intermediate";
+import Advanced from "./Components/Advanced";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [selectedItem, setSelectedItem] = useState(0)
+
+    const data = [
+        {id: 1, title: "Basic", description: "This is the Basic package!"},
+        {id: 2, title: "Intermediate", description: "This is the Intermediate package!"},
+        {id: 3, title: "Advanced", description: "This is the Advanced package!"},
+    ]
+    return (
+        <>
+            <div>
+                <p className={"text-center text-4xl py-8"}>switching levels</p>
+            </div>
+            {data.map((x) => {
+                return (
+                    <div
+                        onClick={() => setSelectedItem(x.id)}
+                        className={!selectedItem ? "border-2 m-2 p-4 text-center hover:bg-amber-300" : "border-2 m-2 p-4 text-center hover:bg-amber-300 text-red"}
+                        key={x.id}>
+                        <p>{x.id}</p>
+                        <p>{x.title}</p>
+                        <p>{x.description}</p>
+                    </div>
+                )
+            })}
+            <div className={"text-center"}>
+                <p>You have item {selectedItem} selected</p>
+                <div className={"text-4xl my-3"}>
+                    {selectedItem === 1 && <Basic/>}
+                    {selectedItem === 2 && <Intermediate/>}
+                    {selectedItem === 3 && <Advanced/>}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
